bc5b8608c909575394bf4fb46039e07500f971cc
BlackFlame33 <1411299332@qq.com>
2019-08-16 18:58:28 +0800

    更新我是编译器：对继承的深度了解。名称不同，或方法类型参数类型不同都不行

---
+++ 第7课 继承与多态/MonsterTestDrive.java
@@ -0,0 +1,32 @@
+public class MonsterTestDrive {
+    public static void main(String[] args) {
+        Monster[] ma = new Monster[3];
+        ma[0] = new Vampire();
+        ma[1] = new Dragon();
+        ma[2] = new Monster();
+        for (int x = 0; x < 3; x++) {
+            ma[x].frighten(x);
+        }
+    }
+}
+
+class Monster {
+    boolean frighten(int d) {
+        System.out.println("arrrgh");
+        return true;
+    }
+}
+
+class Vampire extends Monster {
+    boolean frighten(int x) {
+        System.out.println("a bite?");
+        return false;
+    }
+}
+
+class Dragon extends Monster {
+    boolean frighten(int degree) {
+        System.out.println("breath fire");
+        return true;
+    }
+}
